dd77cd19ff176cc2ddb500070d4faec8d0b1b418
rafael-cr <rafaelnolasco13@gmail.com>
2024-04-11 15:22:09 -0300

    verify duplicate chars

--- main.go
+++ main.go
@@ -10,23 +10,68 @@ const (
 	upper   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	nums    = "0123456789"
 	special = "!@#$%&-_+=.,/?~"
+	debug   = "abcde"
 )
+
+func findDuplicateLetters(slice []byte) map[byte][]int {
+	positions := make(map[byte][]int)
+
+	for i, letter := range slice {
+		if _, ok := positions[letter]; !ok {
+			positions[letter] = []int{i}
+		} else {
+			positions[letter] = append(positions[letter], i)
+		}
+	}
+
+	// Remover a primeira ocorrência de cada letra
+	for letter, pos := range positions {
+		if len(pos) <= 1 {
+			delete(positions, letter)
+		} else {
+			positions[letter] = pos[1:]
+		}
+	}
+
+	return positions
+}
 
 func main() {
 
-	var sizePassw = 12
+	var sizePassw = 5
 	var password = make([]byte, sizePassw)
 	var charSource string
-	charSource = lower + upper + nums + special
+	charSource = debug //lower + upper + nums + special
 	var r = len(charSource) - 1
 
+	//gera a senha
 	for i := 0; i < sizePassw; i++ {
 		randNum := rand.Intn(r)
 		password[i] = byte(randNum)
 	}
 
-	fmt.Println(password)
+	//verifica letras duplicadas
+	mapDup := findDuplicateLetters(password)
+	fmt.Println(mapDup)
 
+	for key, value := range mapDup {
+		fmt.Println(key, value)
+		for i := 0; i < len(value); i++ {
+			var j = value[i]
+			fmt.Println(j)
+			randNum := rand.Intn(r)
+			fmt.Println(randNum)
+			password[j] = byte(randNum)
+			fmt.Println(password[j])
+		}
+	}
+	//for i := 0; i < len(mapDup); i++ {
+	//	value, exists := mapDup[1]
+	//	fmt.Println(exists, value)
+	//}
+
+	//imprime a senha
+	fmt.Println(password)
 	for i := 0; i < len(password); i++ {
 		var j = password[i]
 		fmt.Printf("%c", charSource[j])
